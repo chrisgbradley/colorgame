64107f626072509eff73fd8ce760d2869c4bfeb9
Chris Bradley <chris.bradley.423@gmail.com>
2017-02-11 17:37:38 -0500

    v.0.1
    Laid the framework. Could easily use restructuring in the javascript;

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,90 @@
+var game = {
+    el: document.getElementById("game"),
+    color: document.getElementById("colorCode"),
+    difficulty: findWithClass("selected", document.getElementById("difficulty-list").children),
+    setDifficulty: function(difficulty) {
+        var game = this.el;
+        clearGame();
+        /* a is the length of the desired array*/
+        var a = difficulty+2;
+        for(var i = 0; i < a; i++) {
+            createRow(a);
+        };
+
+
+        function createRow(items) {
+            var node = document.createElement("div");
+            node.classList.add("dc-grid-row");
+
+
+            //append items to row
+            for(var j = 0; j < items; j++) {
+                var subNode = document.createElement("div");
+                subNode.classList.add("dc-grid-item", "square-1-"+items);
+                node.appendChild(subNode);
+            }
+            //append row to game
+            game.appendChild(node);
+        };
+
+        function clearGame() {
+            while (game.hasChildNodes()) {
+                game.removeChild(game.lastChild);
+            };
+        };
+    },
+    initListeners: function() {
+        var e;
+
+        e = document.querySelectorAll("ul li");
+        for(var i = 0; i < e.length; i++){
+            e[i].addEventListener("click", function() {
+                for(var j = 0; j < e.length; j++) {
+                    e[j].classList.remove("selected");
+                };
+                this.classList.add("selected");
+                for(var j = 0; j < e.length; j++) {
+                    if(e[j] === this) {
+                        game.setDifficulty(j);
+                    };
+                };
+            });
+        };
+    },
+    newColors: function() {
+        var tiles = document.querySelectorAll("#game .dc-grid-item");
+        for(var i = 0; i < tiles.length; i++) {
+            tiles[i].style.backgroundColor = rgbCode();
+        };
+
+        function rgbCode() {
+            function rgb() {
+                return Math.floor(Math.random() * 256);
+            };
+            return "rgb(" + rgb() + ", " + rgb() + ", " + rgb() + ")";
+        };
+    },
+    setColor: function() {
+        var tiles = document.querySelectorAll("#game .dc-grid-item");
+        var r = Math.floor(Math.random() * tiles.length);
+        this.color.textContent = tiles[r].style.backgroundColor;
+    }
+};
+
+
+
+
 window.onload = function() {
-    var game = document.getElementById("game");
-    var difficulty = findWithClass("selected", document.getElementById("difficulty-list").children);
-    initGame(game, difficulty);
+    game.setDifficulty(game.difficulty);
+    game.initListeners();
 }
 
-/* FUNCTIONS: */
-// initGame(game, difficulty);
-// findWithClass(cl, arr);
-
-var initGame = function(game, difficulty) {
-
-};
 
 function findWithClass(cl, arr) {
     var e;
-    for(var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         e = arr[i];
-        if(e.classList.contains(cl)) {
+        if (e.classList.contains(cl)) {
             return i;
         }
     }
